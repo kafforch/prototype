0b97f1d336a0f1df6f95ddaccbb6fab0252f0349
scrappycat <mchayat+scrappycatATgithub@gmail.com>
2015-10-08 16:32:12 +1100

    Initial

--- tests/opd_tests.py
+++ tests/opd_tests.py
@@ -4,4 +4,4 @@ from opd import factory
 class OpdTests(unittest.TestCase):
     def test_empty_plan(self):
         plan = factory.create_plan()
-        self.assertTrue(len(plan.get_tasks()) == 0)
+        self.assertTrue( len(plan.get_tasks()) == 0)
