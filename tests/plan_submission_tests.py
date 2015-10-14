595e11fbfbf8b2ecef5be829d726c714d5b4863b
kafforch <mchayat+kafforchATgithub@gmail.com>
2015-10-14 12:13:15 +1100

    Initial

--- tests/plan_submission_tests.py
+++ tests/plan_submission_tests.py
@@ -7,7 +7,11 @@ class PlanSubmissionTestsInit(unittest.TestCase):
         self.plan_repo = plan_repo.PlanRepo()
         self.plan_exec = plan_exec.PlanExec()
         self.plan_parser = plan_parser.PlanParser()
-        self.orchestrator = orchestrator.Orchestrator(self.plan_repo, self.plan_exec, self.plan_parser)
+        self.orchestrator = orchestrator.Orchestrator(
+            plan_repo=self.plan_repo,
+            plan_exec=self.plan_exec,
+            plan_parser=self.plan_parser
+        )
 
 class PlanSubmissionTests(PlanSubmissionTestsInit):
 
