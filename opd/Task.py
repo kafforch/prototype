ae31f97e961d5f17344a88d8ee55123855a110e0
kafforch <mchayat+kafforchATgithub@gmail.com>
2015-10-14 11:30:46 +1100

    Initial

--- opd/Task.py
+++
@@ -1,6 +0,0 @@
-class Task():
-
-    def __init__(self, name, **kwargs):
-        self.name = name
-        for key, value in kwargs.items():
-            self.__dict__[key] = value
--- opd/factory.py
+++
@@ -1,9 +0,0 @@
-from Plan import Plan
-
-def create_plan(tasks=[], dependencies=[]):
-
-    plan = Plan()
-    plan.add_tasks(tasks)
-    plan.add_dependencies(dependencies)
-
-    return plan
