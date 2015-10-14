59f474283d49e1b673022f05075598bf974e739f
kafforch <mchayat+kafforchATgithub@gmail.com>
2015-10-14 16:36:11 +1100

    Initial

--- tests/plan_submission_tests.py
+++ tests/plan_submission_tests.py
@@ -20,19 +20,24 @@ class PlanSubmissionTestsInit(unittest.TestCase):
 
 class PlanSubmissionTests(PlanSubmissionTestsInit):
 
-    def test_submission(self):
+    def test_plan_submission(self):
         plan_json = open('tests/json/plan_01.json', 'r').read()
 
         # callback to simulate a fragment work
         def callback(id, event, data):
             print "Received event {0} for id={1} with data {2}".format(event, id, data)
             print "Sending complete notification"
+            self.assertEqual(data['task_name'], 12)
             self.event_mgr.publish("END_TASK", data)
 
         # simple callback on completion
         def callback_complete(id, event, data):
             print "Task {0} of plan {1} is complete".format(data['task_id'], data['plan_id'])
+            self.assertIn(data['task_id'], [1,2,3])
+            self.assertEqual(data['task_name'], 12)
 
         self.event_mgr.subscribe("123456", "START_TASK", callback)
 
         self.orchestrator.submit_plan_for_execution(plan_json, callback_complete)
+
+        self.event_mgr.unsubscribe("123456", "START_TASK")
