c825255640827345d34080ee78d3a61edc50dd6e
kafforch <kafforch@github.com>
2015-12-17 10:07:09 +1100

    Added testing for running plans

--- tests/orch_tests/plan_submission_tests.py
+++ tests/orch_tests/plan_submission_tests.py
@@ -19,6 +19,9 @@ class MyTestCase(MyTestCaseBase):
         class Test01TaskListener(task_exec.BaseTaskListener):
             def create_listener(self, plan_id, task_id, plan_executor):
                 test_self.assertIn(int(task_id), range(1, 5))
+                plan = plan_repo.get_plan_by_id(plan_id)
+                test_self.assertFalse(plan.is_plan_initial())
+                test_self.assertTrue(plan.is_plan_running())
                 self.task_completed(plan_executor, plan_id, task_id)
 
         orchestrator.execute_plan(plan_id=plan_01_id, task_listener=Test01TaskListener())
