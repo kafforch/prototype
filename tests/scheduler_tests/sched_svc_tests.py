718084009ec8dcaf62952b31f1456cb8110912cd
kafforch <kafforch@github.com>
2015-12-17 21:32:41 +1100

    Added tests for starting timed plans

--- tests/scheduler_tests/sched_svc_tests.py
+++ tests/scheduler_tests/sched_svc_tests.py
@@ -9,15 +9,19 @@ class SchedSvcTests(unittest.TestCase):
     def setUp(self):
         logger = logging.getLogger('scheduler.sched_svc')
         logger.setLevel(logging.DEBUG)
-        plan_repo.purge_all_plans()
-
-    def test_start_timed_plans1(self):
 
         # Save two plans with timed start and one without
+        plan_repo.purge_all_plans()
         plan_repo.save_new_plan(open ( 'tests/scheduler_tests/json/plan_01.json', 'r' ).read ( ))
         plan_repo.save_new_plan(open ( 'tests/scheduler_tests/json/plan_03.json', 'r' ).read ( ))
         plan_repo.save_new_plan(open ( 'tests/scheduler_tests/json/plan_03.json', 'r' ).read ( ))
         plan_repo.save_new_plan(open ( 'tests/scheduler_tests/json/plan_04.json', 'r' ).read ( ))
+
+    def tearDown(self):
+        plan_repo.purge_all_plans()
+
+
+    def test_start_timed_plans1(self):
 
         # Set datetime
         timer_svc.set_datetime("2015-11-12T11:23:44.123Z")
@@ -30,13 +34,6 @@ class SchedSvcTests(unittest.TestCase):
 
     def test_start_timed_plans2(self):
 
-        # Save two plans with timed start and one without. Only one should execute
-        plan_repo.save_new_plan(open ( 'tests/scheduler_tests/json/plan_01.json', 'r' ).read ( ))
-        plan_repo.save_new_plan(open ( 'tests/scheduler_tests/json/plan_03.json', 'r' ).read ( ))
-        plan_repo.save_new_plan(open ( 'tests/scheduler_tests/json/plan_03.json', 'r' ).read ( ))
-        # This one is for a later date
-        plan_repo.save_new_plan(open ( 'tests/scheduler_tests/json/plan_04.json', 'r' ).read ( ))
-
         # Set datetime
         timer_svc.set_datetime("2013-11-12T11:23:44.123Z")
 
@@ -44,3 +41,14 @@ class SchedSvcTests(unittest.TestCase):
             sched_svc.start_timed_plans()
 
         self.assertEqual(mock_execute_plan.call_count, 2)
+
+
+    def test_start_timed_plans3(self):
+
+        # Set datetime
+        timer_svc.set_datetime("2011-11-12T11:23:44.123Z")
+
+        with patch("orch.orchestrator.execute_plan") as mock_execute_plan:
+            sched_svc.start_timed_plans()
+
+        self.assertEqual(mock_execute_plan.call_count, 0)
